4c6508c1ac19a8f0a7157a39ce406091bf397bab
min2oyo <min2u@outlook.com>
2022-06-13 16:30:12 +0900

    url 들고오기

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
-const home = () => {
+const Home = () => {
+
+  useEffect(() => {
+
+  }, []);
+
   return (
-    <div>home</div>
+    <div>Home</div>
   );
 };
 
-export default home;
+export default Home;
