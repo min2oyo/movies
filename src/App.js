3db3982557069b667f0ce0af785390081230cba1
min2oyo <min2u@outlook.com>
2022-06-13 16:14:52 +0900

    상단메뉴

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import { Route, Routes } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Home from './pages/Home';
 import Movies from './pages/Movies';
 import MovieDetail from './pages/MovieDetail';
+import Navigation from './components/Navigation';
 
 function App() {
   return (
     <>
+      <Navigation />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/movies' element={<Movies />} />
---
+++ src/components/Navigation.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Navbar, Container, Nav, NavDropdown, Form, FormControl, Button } from 'react-bootstrap';
+
+const Navigation = () => {
+  return (
+    <>
+      <Navbar bg="dark" variant="dark" expand="lg">
+        <Container fluid>
+          <Navbar.Brand href="#">
+            <img src='https://media.phillyvoice.com/media/images/Netflix-Logo-Print_CMYK.2e16d0ba.fill-735x490.jpg' alt='netflix logo' width={100} />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll">
+            <Nav
+              className="me-auto my-2 my-lg-0"
+              style={{ maxHeight: '100px' }}
+              navbarScroll
+            >
+              <Link to='/' className='nav-item'>Home</Link>
+              <Link to='/movies' className='nav-item'>Movies</Link>
+            </Nav>
+            <Form className="d-flex">
+              <FormControl
+                type="search"
+                placeholder="Search"
+                className="me-2"
+                aria-label="Search"
+              />
+              <Button variant="outline-danger">Search</Button>
+            </Form>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
+  );
+};
+
+export default Navigation;
