1aa881c752fad1e92eb98d542781459afea5b0c3
min2oyo <min2u@outlook.com>
2022-06-13 15:38:34 +0900

    라우터 세팅

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './pages/Home';
+import Movies from './pages/Movies';
+import MovieDetail from './pages/MovieDetail';
 
 function App() {
   return (
     <>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/movies' element={<Movies />} />
+        <Route path='/movies/:id' element={<MovieDetail />} />
+      </Routes>
     </>
   );
 }
