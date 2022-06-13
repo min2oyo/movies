4f0da550012f208aaa0317fc60ef4d357bb21a91
min2oyo <min2u@outlook.com>
2022-06-13 19:25:52 +0900

    Promise

--- src/redux/action/movieAction.js
+++ src/redux/action/movieAction.js
@@ -4,31 +4,16 @@ const API_KEY = process.env.REACT_APP_API_KEY;
 
 function getMovies() {
   return async dispatch => {
-    const popularMovieApi = await api.get(`/movie/popular?api_key=${API_KEY}&language=en-US&page=1`);
+    const popularMovieApi = api.get(`/movie/popular?api_key=${API_KEY}&language=en-US&page=1`);
+    const topRateApi = api.get(`/movie/top_rated?api_key=${API_KEY}&language=en-US&page=1`);
+    const upComingApi = api.get(`/movie/upcoming?api_key=${API_KEY}&language=en-US&page=1`);
 
-
-    // let url2 = `https://api.themoviedb.org/3/movie/top_rated?api_key=<<api_key>>&language=en-US&page=1`;
-    // let response2 = await fetch(url2);
-
-    // let url3 = `https://api.themoviedb.org/3/movie/upcoming?api_key=<<api_key>>&language=en-US&page=1`;
-    // let response3 = await fetch(url3);
+    let [popularMovies, topRateMovies, upComingMovies] = await Promise.all([popularMovieApi, topRateApi, upComingApi]);
+    console.log(popularMovies);
+    console.log(topRateMovies);
+    console.log(upComingMovies);
   };
 };
-
-
-// function getMovies() {
-//   return dispatch => async {
-//     let url = `https://api.themoviedb.org/3/movie/popular?api_key=<<api_key>>&language=en-US&page=1`;
-//     let response = await fetch(url);
-//     let data = await response.json();
-
-//     let url2 = `https://api.themoviedb.org/3/movie/top_rated?api_key=<<api_key>>&language=en-US&page=1`;
-//     let response2 = await fetch(url2);
-
-//     let url3 = `https://api.themoviedb.org/3/movie/upcoming?api_key=<<api_key>>&language=en-US&page=1`;
-//     let response3 = await fetch(url3  );
-//   };
-// }
 
 export const movieAction = {
   getMovies,
